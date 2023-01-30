39f1a9b938f0433f8e80ba86bac8a5bb1f82ef39
Josh Hancock <livingicon@gmail.com>
2023-01-30 17:19:44 -0500

    Add randomize method to Cards functional component to shuffle the pokeArr

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,27 @@
 // Cards.js
 
-import React from 'react'
+import React, { useState } from 'react';
 
 const Cards = () => {
 
+  const [ pokeArr, setPokeArr ] = useState(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+ 
+  const randomize = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+ 
+  const shuffleArr = () => {
+    setPokeArr(pokeArr.concat(randomize(pokeArr)))
+  }
   return (
     <div className="cards">
-      <h3>all the cards</h3>
+      <div>{pokeArr}</div>
+      <button onClick={shuffleArr}>Shuffle Array</button>
     </div>
   )
 }
