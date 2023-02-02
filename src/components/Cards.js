91580c08752e7ce7deafac2ae030568bc064dd17
Josh Hancock <livingicon@gmail.com>
2023-02-01 20:52:56 -0500

    Successfully add pokemonAPI fetch useEffect

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,24 +4,24 @@ import React, { useState, useEffect } from 'react';
 
 const Cards = () => {
 
-  // const [ pokeArr, setPokeArr ] = useState(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+  // const [ array, setArray ] = useState(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
   const [ pokeArr, setPokeArr ] = useState([]);
 
   useEffect(() => {
-    const loadCards = async () => {
-      setPokeArr(await fetchPoke(6)) // add randomize here before await?
+    const displayCards = async () => {
+      setPokeArr(randomize(await fetchPoke(12))) // add randomize here before await?
     }
-    loadCards()
+    displayCards();
   }, [])
 
-  // const randomize = (arr) => {
-  //   for (let i = arr.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     const temp = arr[i];
-  //     arr[i] = arr[j];
-  //     arr[j] = temp;
-  //   }
-  // }
+  const randomize = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
 
   const fetchPoke = async (num) => {
     const pokemons = []
@@ -30,26 +30,21 @@ const Cards = () => {
       const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
       const response = await fetch(pokeUrl);
       const pokemon = await response.json();
-      console.log(pokemon);
-      
-    //   const id = pokemon.id;
-    //   const name = pokemon.name;
-    //   const image = pokemon.sprites.front_default;
-    //   pokemons.push({ id, name, image });
+      const pokeId = pokemon.id;
+      const pokeName = pokemon.name;
+      const pokeImage = pokemon.sprites.front_default;
+      pokemons.push({ pokeId, pokeName, pokeImage });
     }
-
-    return pokemons
+    console.log(pokemons);
+    return pokemons;
   }
- 
-  // const shuffleArr = () => {
-  //   setPokeArr(pokeArr.concat(randomize(pokeArr)))
-  // }
 
   return (
     <div className="cards">
       {/* <div>{pokeArr}</div>
       <button onClick={shuffleArr}>Shuffle Array</button> */}
-      <button onClick={fetchPoke}>Console PokeAPI</button>
+      {/* below isn't doing anything */}
+      {/* <button onClick={fetchPoke}>Console PokeAPI</button>  */}
     </div>
   )
 }
