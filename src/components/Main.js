104c867f2d3ae64f51758c79480bf2a8eb705d5d
Josh Hancock <livingicon@gmail.com>
2023-02-05 14:12:56 -0500

    Add utilities.js folder and move randomize function to it

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,53 +3,44 @@
 import React, { useState, useEffect } from 'react';
 import PokeScores from './pokeScores/PokeScores';
 import PokeCards from './pokeCards/PokeCards';
+import { randomize } from '../utilities';
 
 const Main = () => {
-
   const [ pokeArr, setPokeArr ] = useState([]);
   const [currentScore, setCurrentScore] = useState(0)
 
+
   useEffect(() => {
     const displayCards = async () => {
-      setPokeArr(randomize(await fetchPoke(5))) // add randomize here before await?
+      setPokeArr(randomize(await fetchPoke(12)))
     }
     displayCards();
   }, [])
 
-  const randomize = (arr) => {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    return arr;
-  }
 
   const fetchPoke = async (num) => {
     const tempArr = [];
-    
     for (let i = 1; i <= num; i++) {
       const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
       const response = await fetch(pokeUrl);
-      const pokemon = await response.json(); //pokemon (all the data)
+      const pokemon = await response.json();
       const pokeId = pokemon.id;
       const pokeName = pokemon.name;
       const pokeImage = pokemon.sprites.front_default;
       tempArr.push({ pokeId, pokeName, pokeImage });
     }
     return tempArr;
-    // setPokeArr(tempArr);
   }
+
 
   return (
     <div className='main'>
       <PokeScores 
         currentScore={currentScore}
       />
-      {pokeArr && <PokeCards 
-        pokeArr={pokeArr} // conditionally render PokeCards
-      />}
+      <PokeCards 
+        pokeArr={pokeArr}
+      />
     </div>
   )
 }
